f59317c5cea1b07cd740a616982bc44ed849c025
Jonathan Chang <jchang641@gmail.com>
2012-05-09 12:17:36 -0700

    Add velvet

---
+++ velvet.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class Velvet < Formula
+  version '1.2.05'
+  homepage 'http://www.ebi.ac.uk/~zerbino/velvet/'
+  # doesn't seem to have stable tarballs
+  url 'https://github.com/dzerbino/velvet.git', :tag => 'aeb11f8058e4ea794a6ec425c168ffcbbfd1bbbc'
+  head 'https://github.com/dzerbino/velvet.git'
+
+  def install
+    inreplace 'Makefile' do |s|
+      # recommended in Makefile for compiling on Mac OS X
+      s.change_make_var! "CFLAGS", "-Wall -m64"
+    end
+    system "make MAXKMERLENGTH=96"
+    bin.install 'velveth', 'velvetg'
+  end
+end
