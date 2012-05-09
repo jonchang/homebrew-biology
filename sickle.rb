57e7d3d184ee84f14d056f301aeaf62e51200675
Jonathan Chang <jchang641@gmail.com>
2012-05-09 12:00:19 -0700

    Add sickle

---
+++ sickle.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+class Sickle < Formula
+  version '1.2'
+  homepage 'https://github.com/najoshi/sickle'
+  url 'https://github.com/najoshi/sickle.git', :tag => '032bf41f962ffeacca9a21ba222b6238760bc1cf'
+  head 'https://github.com/najoshi/sickle.git'
+
+  def install
+    system "make"
+    bin.install "sickle"
+  end
+end
