57061deedaf1df2655e1f82ec5387feee87184b8
Jonathan Chang <jchang641@gmail.com>
2012-05-09 11:51:51 -0700

    Update scythe

--- scythe.rb
+++ scythe.rb
@@ -1,15 +1,13 @@
 require 'formula'
 
 class Scythe < Formula
+  version '0.981'
   homepage 'https://github.com/vsbuffalo/scythe'
   head 'https://github.com/vsbuffalo/scythe.git'
+  url 'https://github.com/vsbuffalo/scythe.git', :tag => '5cef1e9a0e3fce8ad9c5f084e88993fde6a3f361'
 
   def install
     system "make build"
     bin.install "scythe"
-  end
-
-  def test
-    system "make test"
   end
 end
