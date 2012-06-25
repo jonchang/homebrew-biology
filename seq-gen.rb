f2715450f47cef5d9600bf4d0a2e1f4f5cbcd830
Jonathan Chang <jchang641@gmail.com>
2012-06-25 04:05:42 -0700

    New formula: seq-gen

---
+++ seq-gen.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class SeqGen < Formula
+  homepage 'http://tree.bio.ed.ac.uk/software/seqgen/'
+  url 'http://tree.bio.ed.ac.uk/download.php?id=85&num=1'
+  sha1 'b96d353c963766d78bde47761bc6cecd6dae0372'
+  version '1.3.3'
+
+  def install
+    cd "source" do
+      system "make"
+      bin.install "seq-gen"
+    end
+  end
+end
