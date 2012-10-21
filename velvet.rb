6a180e5c26f909ba82a905cf3c2df44b410507b1
Jonathan Chang <jchang641@gmail.com>
2012-10-21 01:22:00 -0700

    velvet: 1.2.08

--- velvet.rb
+++ velvet.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Velvet < Formula
-  version '1.2.07'
   homepage 'http://www.ebi.ac.uk/~zerbino/velvet/'
-  url 'https://github.com/dzerbino/velvet.git', :revision => 'bfaff26f78f1777e338953267ae9cc9f84039a22'
+  url 'http://www.ebi.ac.uk/~zerbino/velvet/velvet_1.2.08.tgz'
+  sha1 'cef486759fa577d86ff67d70898ff330f5d0403c'
   head 'https://github.com/dzerbino/velvet.git'
 
   # patch per comment in Makefile
