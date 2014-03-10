d56a820cc293f9b0bc3202e631c42c976daa9ceb
Jonathan Chang <jchang641@gmail.com>
2014-03-10 00:37:46 -0700

    bamm: trick out formula, because why not

--- bamm.rb
+++ bamm.rb
@@ -1,26 +1,75 @@
 require "formula"
+require "requirement"
+
+class RRequirement < Requirement
+  fatal true
+  satisfy { which "R" }
+  def message; "Install R with `brew install homebrew/science/r`"; end
+end
+
+class ApeRequirement < Requirement
+  fatal true
+  satisfy { system "Rscript", "-e", "library(ape)" }
+  def message; <<-EOS.undent
+    Install ape with:
+       Rscript -e 'install.packages("ape")'
+    EOS
+  end
+end
 
 class Bamm < Formula
   homepage "http://bamm-project.org/"
-  head "https://github.com/macroevolution/bamm.git"
+
+  option "without-bammtools", "don't install bammtools (requires R)"
+  depends_on ApeRequirement if build.with? "bammtools"
+  depends_on RRequirement if build.with? "bammtools"
+
+  stable do
+    url "http://www-personal.umich.edu/~carlosja/bamm-1.0.0.tar.gz"
+    sha1 "1f14901a40931d6de208d3b17b0031b165c87de8"
+
+    resource "examples" do
+      url "http://www-personal.umich.edu/~carlosja/bamm-examples.tar.gz"
+      sha1 "b26777e04bad5a17426c453278bfae75e74ee6e4"
+    end
+
+    resource "bammtools" do
+      url "http://www-personal.umich.edu/~carlosja/BAMMtools_1.0.1.tar.gz"
+      sha1 "dcf506982245414ed5f2f2c98e9572304d122ca1"
+    end
+  end
+
+  head do
+    url "https://github.com/macroevolution/bamm.git"
+
+    resource "bammtools" do
+      url "https://github.com/macroevolution/bammtools.git"
+    end
+  end
 
   depends_on "cmake" => :build
 
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
-    share.install "doc", "examples"
+
+    if build.head?
+      resource("bammtools").stage { system "R", "CMD", "INSTALL", "BAMMtools" } if build.with? 'bammtools'
+      share.install Dir["examples/*"]
+    else
+      resource("bammtools").stage { system "R", "CMD", "INSTALL", "." } if build.with? 'bammtools'
+      resource("examples").stage { share.install Dir['*'] }
+    end
   end
 
   def caveats; <<-EOS.undent
-  Documentation and examples have been installed to:
-
-    #{opt_prefix}/share/docs
-    #{opt_prefix}/share/examples
-
-  To install the R package, start R then:
-
-    install.packges("BAMMtools")
+    Examples have been installed to:
+      #{share}
   EOS
+  end
+
+  def test
+    # Doesn't actually work, lol
+    system "bamm", "-c", share + "diversification/anoles/divcontrol.txt"
   end
 end
