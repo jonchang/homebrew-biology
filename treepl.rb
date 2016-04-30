fbe2c0c71bef0fe1272839b02c39f0a401dfbdc7
Jonathan Chang <jchang641@gmail.com>
2016-04-30 15:59:11 -0700

    new formula: treePL

---
+++ treepl.rb
@@ -0,0 +1,83 @@
+class Treepl < Formula
+  desc "dating phylogenies with penalized likelihood"
+  homepage "http://blackrim.org/programs/treepl/"
+  head "https://github.com/blackrim/treePL.git"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "nlopt"
+
+  needs :openmp
+
+  patch do
+    url "https://patch-diff.githubusercontent.com/raw/blackrim/treePL/pull/19.patch"
+    sha256 "96b17a2ee4f232fc7b1e591ed4e7074268e16992992e2e12f516e1d44d31b670"
+  end
+
+  # Check for brewed adol-c
+  adolc = Formula["adol-c"]
+  if adolc.installed? and Keg.for(adolc.lib).linked?
+    onoe "Please unlink Homebrew's ADOL-C before installing treePL:"
+    onoe "    brew unlink adol-c"
+    onoe "    brew install treepl"
+    odie "    brew link adol-c"
+  end
+
+  def install
+
+    # Use the vendored copy of adol-c since Homebrew's version doesn't support OMP
+    cd "deps" do
+      system "tar", "xf", "adol-c_git_saved.tar.gz"
+      cd "adol-c" do
+        system "autoreconf", "-fi"
+        system "./configure", "--with-openmp-flag=-fopenmp", "--prefix=#{libexec}"
+        system "make", "install"
+      end
+    end
+
+    cd "src" do
+      # Tell configure where to find libraries
+      nlopt = Formula["nlopt"]
+      ENV.append "CPPFLAGS", "-I#{nlopt.include}"
+      ENV.append "CPPFLAGS", "-I#{libexec}/include"
+      ENV.append "CFLAGS", "-I#{nlopt.include}"
+      ENV.append "LDFLAGS", "-L#{nlopt.lib}"
+      ENV.append "LDFLAGS", "-L#{libexec}/lib64"
+      ENV.append "LIBS", "-lnlopt_cxx"
+
+      # Makefile ignores everything configure tells it so...
+      inreplace "Makefile.in" do |s|
+        s.gsub! "-lnlopt", "-lnlopt_cxx"
+      end
+
+      system "./configure"
+      system "make"
+      bin.install "treePL"
+    end
+
+    pkgshare.install "examples"
+  end
+
+  def caveats; <<-EOS.undent
+    If you encounter build problems and also have ADOL-C installed, unlink it
+    before attempting to build treePL:
+
+        brew unlink adol-c
+        brew install treepl
+        brew link adol-c
+
+    Example files were installed to:
+        #{opt_prefix}/share/treepl
+    EOS
+  end
+
+  test do
+    cp_r opt_pkgshare/"examples", testpath
+    cd testpath/"examples" do
+        Dir["*.cppr8s"].each do |cfg|
+          system "#{bin}/treePL", cfg
+        end
+    end
+  end
+end
