c3e853e284719a4732ac24220c9c3bc05adcc720
Jonathan Chang <jchang641@gmail.com>
2016-08-26 08:46:47 -0400

    beast2: v2.4.3

--- beast2.rb
+++ beast2.rb
@@ -1,38 +1,39 @@
-require "formula"
-
 class Beast2 < Formula
+  desc "Bayesian evolutionary analysis by sampling trees"
   homepage "http://www.beast2.org/"
-  url "https://github.com/CompEvol/beast2/archive/v2.1.1.tar.gz"
-  sha1 "6b97d7bfd1428c4024cf3ddd5a2d62f63b50f721"
+  url "https://github.com/CompEvol/beast2/archive/v2.4.3.tar.gz"
+  sha256 "aa684f57ec38b51a00dcf20c16a0835de0e3920e55ba811e126ed98f6b182df1"
   head "https://github.com/CompEvol/beast2.git"
+  # tag "bioinformatics"
+  # doi "10.1371/journal.pcbi.1003537"
 
   depends_on :ant => :build
+  depends_on :java => "1.8+"
 
   def patches
     # Homebrew renames the unpacked source folder, but build.xml
-    # assumes that  it won't be renamed.
+    # assumes that it won't be renamed.
     DATA
   end
 
   def install
     system "ant", "linux"
-    cd 'release/Linux/BEAST' do
+    cd "release/Linux/beast" do
       inreplace Dir["bin/*"] do |s|
-        s['$BEAST/lib'] = '$BEAST/libexec'
+        s["$BEAST/lib"] = "$BEAST/libexec"
       end
 
-      Dir['bin/*'].each do |f|
+      Dir["bin/*"].each do |f|
         mv f, f + "-2"
       end
 
-      mv 'lib', 'libexec'
+      mv "lib", "libexec"
       prefix.install Dir["*"]
     end
   end
 
   def caveats; <<-EOS.undent
-    This installation can coexist with other installs of BEAST.
-    All scripts are suffixed with '-2':
+    This install coexists with BEASTv1 as all scripts are suffixed with '-2':
 
         beast-2 -help
   EOS
@@ -44,23 +45,47 @@ class Beast2 < Formula
 end
 __END__
 diff --git a/build.xml b/build.xml
-index 1940dcc..efde88b 100644
+index 420fe49..64b8bc5 100644
 --- a/build.xml
 +++ b/build.xml
-@@ -8,11 +8,11 @@
- 	</description>
+@@ -8,12 +8,12 @@
+     </description>
  
- 	<!-- set global properties for this build -->
--	<property name="src" location="../beast2/src" />
--	<property name="build" location="../beast2/build" />
--	<property name="lib" location="../beast2/lib" />
--	<property name="doc" location="../beast2/doc" />
--	<property name="dist" location="../beast2/build/dist" />
-+	<property name="src" location="src" />
-+	<property name="build" location="build" />
-+	<property name="lib" location="lib" />
-+	<property name="doc" location="doc" />
-+	<property name="dist" location="build/dist" />
+     <!-- set global properties for this build -->
+-    <property name="src" location="../beast2/src" />
+-    <property name="build" location="../beast2/build" />
+-    <property name="lib" location="../beast2/lib" />
+-    <property name="doc" location="../beast2/doc" />
+-    <property name="dist" location="../beast2/build/dist" />
+-    <property name="test" location="../beast2/test" />
++    <property name="src" location="src" />
++    <property name="build" location="build" />
++    <property name="lib" location="lib" />
++    <property name="doc" location="doc" />
++    <property name="dist" location="build/dist" />
++    <property name="test" location="test" />
  
- 	<property name="main_class_BEAST" value="beast.app.beastapp.BeastMain" />
- 	<property name="report" value="build/junitreport" />
+     <property name="main_class_BEAST" value="beast.app.beastapp.BeastMain" />
+     <property name="report" value="build/junitreport" />
+@@ -161,7 +161,7 @@
+         <mkdir dir="${release_dir}/package/lib" />
+         <copy file="${dist}/beast.src.jar" todir="${release_dir}/package/"/>
+         <copy file="${dist}/beast.jar" todir="${release_dir}/package/lib/"/>
+-        <copy file="../beast2/version.xml" todir="${release_dir}/package/"/>
++        <copy file="version.xml" todir="${release_dir}/package/"/>
+ 
+         <jar jarfile="${release_dir}/package/BEAST.addon.v${version}.zip">
+             <fileset dir="${release_dir}/package">
+@@ -465,11 +465,10 @@
+         
+         <echo message="cd release/Linux"/>
+         <echo message="tar fcz BEAST.v${version}.tgz beast"/>
+-        <echo message="cp BEAST.v${version}.tgz ~/tmp"/>
+     </target>
+ 
+     <!-- Define the appbundler task -->
+-    <taskdef name="bundleapp" classname="com.oracle.appbundler.AppBundlerTask" classpath="../beast2/lib/bundler/appbundler-1.0.jar"/>
++    <taskdef name="bundleapp" classname="com.oracle.appbundler.AppBundlerTask" classpath="lib/bundler/appbundler-1.0.jar"/>
+ 
+     <!--property name="AppleSigner" value="Developer ID Application: Alexei Drummond (6M6Y6L7RUP)" /-->
+     <property name="AppleSigner" value="Developer ID Application: Remco Bouckaert (LHFJWE5U63)" />    
