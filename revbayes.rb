bca15a5b0be55f0a3350022eadf0e84746e3a71b
Jonathan Chang <jchang641@gmail.com>
2016-08-26 12:09:25 -0400

    revbayes: this hopefully works

--- revbayes.rb
+++ revbayes.rb
@@ -14,7 +14,7 @@ class Revbayes < Formula
       system "./regenerate.sh", "-boost", "false"
       system "cmake", ".", *std_cmake_args
       system "make"
-      bin.install ["rb", "rb-extended"]
+      bin.install "rb"
     end
   end
 
@@ -22,18 +22,17 @@ class Revbayes < Formula
     system "false"
   end
 end
-
 __END__
 diff --git a/projects/cmake/regenerate.sh b/projects/cmake/regenerate.sh
-index 0872688..b3840ce 100755
+index 46b128c..fe14211 100755
 --- a/projects/cmake/regenerate.sh
 +++ b/projects/cmake/regenerate.sh
-@@ -112,7 +112,6 @@ set(PROJECT_SOURCE_DIR ${CMAKE_SOURCE_DIR}/../../src)
+@@ -161,7 +161,6 @@ set(PROJECT_SOURCE_DIR ${CMAKE_SOURCE_DIR}/../../src)
  
  
  
--SET(BOOST_ROOT ../../boost_1_55_0)
+-SET(BOOST_ROOT ../../boost_1_60_0)
  SET(Boost_USE_STATIC_RUNTIME true)
- #find_package(Boost 1.55.0 COMPONENTS filesystem regex signals context system thread date_time program_options iostreams serialization math_c99 math_c99f math_tr1f math_tr1l REQUIRED)
- find_package(Boost
+ SET(Boost_USE_STATIC_LIBS ON)
+ #find_package(Boost 1.60.0 COMPONENTS filesystem regex signals system thread date_time program_options serialization math_c99 math_c99f math_tr1f math_tr1l REQUIRED)
 
