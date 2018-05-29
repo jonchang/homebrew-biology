47a94b1dc4d294648a5b2f66f78d52f19377d598
Jonathan Chang <jchang641@gmail.com>
2018-05-28 20:34:44 -0400

    phylonet

---
+++ Formula/phylonet.rb
@@ -0,0 +1,18 @@
+class Phylonet < Formula
+  desc "Reconstructing phylogenetic networks"
+  homepage "https://bioinfocs.rice.edu/phylonet"
+  url "https://bioinfocs.rice.edu/sites/g/files/bxs266/f/kcfinder/files/PhyloNet_3.6.4.jar", :using => :nounzip
+  sha256 "3919c3408fb5f7af0d465738475cc20d2952eba37e09c51ee3f3ba8df966a8d4"
+
+  bottle :unneeded
+  depends_on :java
+
+  def install
+    libexec.install Dir["*.jar"]
+    bin.write_jar_script Dir[libexec/"*.jar"][0], "phylonet"
+  end
+
+  test do
+    system "#{bin}/phylonet"
+  end
+end
