6f54d9e37e6fcec88a8dfe657ac349593f105122
Jonathan Chang <jchang641@gmail.com>
2018-06-13 16:55:02 -0700

    revbayes 1.0.8

--- Formula/revbayes.rb
+++ Formula/revbayes.rb
@@ -1,9 +1,8 @@
 class Revbayes < Formula
   desc "Bayesian phylogenetic inference with graphical models"
   homepage "https://revbayes.github.io/"
-  url "https://github.com/revbayes/revbayes/archive/v1.0.1-release.tar.gz"
-  version "1.0.1"
-  sha256 "38aa1879da8b886cbc66925ca593fa5d25b2e746f8163298623a1871dcec105f"
+  url "https://github.com/revbayes/revbayes/archive/v1.0.8.tar.gz"
+  sha256 "7029ef28bad036a813c3753ca421f731a2840928b94dfc2b8b817dfc83745c61"
   head "https://github.com/revbayes/revbayes.git", :branch => "development"
   # tag "bioinformatics"
   # doi "10.1093/sysbio/syw021"
@@ -12,24 +11,17 @@ class Revbayes < Formula
   depends_on "open-mpi"
   depends_on "boost"
 
-  patch :DATA
-
   def install
     cd "projects/cmake" do
+      inreplace "regenerate.sh", /^SET.*BOOST_ROOT.*$/, ""
+      mkdir "build"
       system "./regenerate.sh", "-boost", "false"
-      system "cmake", ".", *std_cmake_args
-      system "make"
+      cd "build" do
+        system "cmake", ".", *std_cmake_args
+        system "make"
+      end
       bin.install "rb"
     end
-
-    pkgshare.install "examples", "tests"
-  end
-
-  def caveats
-    <<-EOS.undent
-    Example files are installed to:
-      #{pkgshare}
-    EOS
   end
 
   test do
@@ -39,17 +31,3 @@ class Revbayes < Formula
     system "#{bin}/rb", "HKY.Rev"
   end
 end
-__END__
-diff --git a/projects/cmake/regenerate.sh b/projects/cmake/regenerate.sh
-index 46b128c..fe14211 100755
---- a/projects/cmake/regenerate.sh
-+++ b/projects/cmake/regenerate.sh
-@@ -161,7 +161,6 @@ set(PROJECT_SOURCE_DIR ${CMAKE_SOURCE_DIR}/../../src)
- 
- 
- 
--SET(BOOST_ROOT ../../boost_1_60_0)
- SET(Boost_USE_STATIC_RUNTIME true)
- SET(Boost_USE_STATIC_LIBS ON)
- #find_package(Boost 1.60.0 COMPONENTS filesystem regex signals system thread date_time program_options serialization math_c99 math_c99f math_tr1f math_tr1l REQUIRED)
-
