9ac4b7ca9a06587889ec032634064df32e8790f4
Jonathan Chang <jchang641@gmail.com>
2018-06-13 17:03:10 -0700

    Fix phylobayes-mpi

--- Formula/phylobayes-mpi.rb
+++ Formula/phylobayes-mpi.rb
@@ -1,7 +1,8 @@
 class PhylobayesMpi < Formula
   desc "Phylogenetic reconstruction using infinite mixtures"
   homepage "http://megasun.bch.umontreal.ca/People/lartillot/www/downloadmpi.html"
-  head "https://github.com/bayesiancook/pbmpi.git"
+  url "https://github.com/bayesiancook/pbmpi.git", :revision => "c509b221f9dd29a456efe2ec74dea316f4dd3200"
+  version "2018.03.08"
 
   depends_on "open-mpi"
 
@@ -16,7 +17,7 @@ class PhylobayesMpi < Formula
     pkgshare.install "pb_mpiManual1.7.pdf"
   end
 
-  def caveats; <<-EOS.undent
+  def caveats; <<~EOS
     The manual has been installed to:
       #{opt_pkgshare}
     EOS
