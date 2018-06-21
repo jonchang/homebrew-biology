6236f2b4e4d072a1e7f4213541add5264d72875b
Jonathan Chang <jchang641@gmail.com>
2018-06-21 13:43:47 -0400

    lagrange-cpp (new formula)

---
+++ Formula/lagrange-cpp.rb
@@ -0,0 +1,20 @@
+class LagrangeCpp < Formula
+  desc "the c++ version of lagrange"
+  homepage "http://code.google.com/p/lagrange"
+  url "https://github.com/rhr/lagrange-cpp.git", :revision => "7d9eaa6322c59ac1283e976a37861a3101abb9cc"
+  version "2018.05.15"
+  head "https://github.com/rhr/lagrange-cpp.git"
+
+  depends_on "gcc"
+
+  fails_with :clang
+
+  def install
+    system "make", "-C", "src", "FC=gfortran"
+    bin.install "src/lagrange_cpp"
+  end
+
+  test do
+    system "false"
+  end
+end
