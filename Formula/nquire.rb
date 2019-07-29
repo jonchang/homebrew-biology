86f0dfa23585d8db94903f75f46b39b4c24f74b5
Jonathan Chang <jchang641@gmail.com>
2019-07-29 15:26:58 +1000

    nquire: update 2
    This reverts commit 5f49688d477093e3b8ec0c6cb34c5c3b0965f100.

--- Formula/nquire.rb
+++ Formula/nquire.rb
@@ -2,8 +2,7 @@ class Nquire < Formula
   # cite Weiss_2018: "https://doi.org/10.1186/s12859-018-2128-z"
   desc "Ploidy estimation using NGS short-read data"
   homepage "https://github.com/clwgg/nQuire"
-  url "https://github.com/clwgg/nQuire/archive/a990a88ef14b38f257f1a0d368ba8be1bd3d7e4b.zip"
-  sha256 "0e768896a9e700205249b4c330b0923f51d9d4d5c032743cad78f52fd80c9da5"
+  url "https://github.com/clwgg/nQuire.git", :revision => "a990a88ef14b38f257f1a0d368ba8be1bd3d7e4b"
   version "2018.5.4"
   head "https://github.com/clwgg/nQuire.git"
 
