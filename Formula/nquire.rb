e869fdf159ace55a3271223f6449d381cfa9cef1
Jonathan Chang <jchang641@gmail.com>
2019-07-29 15:22:06 +1000

    nQuire (new formula)

---
+++ Formula/nquire.rb
@@ -0,0 +1,19 @@
+class Nquire < Formula
+  desc "Ploidy estimation using NGS short-read data"
+  homepage "https://github.com/clwgg/nQuire"
+  url "https://github.com/clwgg/nQuire.git", :revision => "a990a88ef14b38f257f1a0d368ba8be1bd3d7e4b"
+  version "2018.5.4"
+  head "https://github.com/clwgg/nQuire.git"
+
+  depends_on "htslib"
+  uses_from_macos "zlib"
+
+  def install
+    system "make"
+    bin.install "nQuire"
+  end
+
+  test do
+    system "#{bin}/nQuire"
+  end
+end
