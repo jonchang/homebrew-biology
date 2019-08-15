7fd7d5b36cdf308ce8ea91ffba2051e7b7d1a263
Jonathan Chang <jchang641@gmail.com>
2019-08-15 13:29:00 +1000

    Delete main.workflow.sh

--- .github/main.workflow.sh
+++
@@ -1,9 +0,0 @@
-set -e
-brew update
-HOMEBREW_TAP_DIR="/usr/local/Homebrew/Library/Taps/jonchang/homebrew-biology"
-mkdir -p "$HOMEBREW_TAP_DIR"
-rm -rf "$HOMEBREW_TAP_DIR"
-ln -s "$PWD" "$HOMEBREW_TAP_DIR"
-if [ -z "$SYSTEM_PULLREQUEST_TARGETBRANCH" ]; then export SYSTEM_PULLREQUEST_TARGETBRANCH="origin/master"; fi
-brew test-bot --tap="jonchang/biology" --root-url=https://dl.bintray.com/jonchang/bottles-biology
-
