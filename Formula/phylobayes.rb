01738f90c318e8cbb4f88d4886f012c30ad93a35
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-11-13 21:56:42 +0000

    phylobayes: add 4.1c_1 bottle.

--- Formula/phylobayes.rb
+++ Formula/phylobayes.rb
@@ -4,6 +4,13 @@ class Phylobayes < Formula
   url "http://megasun.bch.umontreal.ca/People/lartillot/www/phylobayes4.1c.tar.gz"
   version "4.1c"
   sha256 "3ab7e853d720537aeeae5c50605abb8559e24221dbc36d97aa02a31753ace943"
+  bottle do
+    root_url "https://dl.bintray.com/jonchang/bottles-biology"
+    cellar :any_skip_relocation
+    sha256 "9488c612b4be72bd5c1d00a993a88411f5ab22409c518ce0c67b0bac0ca3405d" => :catalina
+    sha256 "c6c9b52153bfb8c7c49f2b94326a7187e3edf4ed56442af0045f9700ac843f67" => :x86_64_linux
+  end
+
   # tag "bioinformatics"
   # doi "10.1093/bioinformatics/btp368"
 
