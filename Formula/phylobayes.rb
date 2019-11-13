e3870c4e4e530fe13d943dce6b9920224b896d34
Jonathan Chang <jchang641@gmail.com>
2019-11-13 23:29:14 +1100

    phylobayes: patch for clang

--- Formula/phylobayes.rb
+++ Formula/phylobayes.rb
@@ -1,12 +1,11 @@
 class Phylobayes < Formula
+  # cite Lartillot_2009: "https://doi.org/10.1093/bioinformatics/btp368"
   desc "Phylogenetic reconstruction using infinite mixtures"
   homepage "http://megasun.bch.umontreal.ca/People/lartillot/www/download.html"
   url "http://megasun.bch.umontreal.ca/People/lartillot/www/phylobayes4.1c.tar.gz"
   version "4.1c"
   sha256 "3ab7e853d720537aeeae5c50605abb8559e24221dbc36d97aa02a31753ace943"
   revision 1
-  # tag "bioinformatics"
-  # doi "10.1093/bioinformatics/btp368"
 
   conflicts_with "phylobayes-mpi"
 
@@ -14,7 +13,8 @@ class Phylobayes < Formula
 
   def install
     system "make", "-C", "sources"
-    bin.install Dir["data/*"]
+    libexec.install Dir["data/*"]
+    bin.install_symlink Dir["#{libexec}/*"] - Dir["#{libexec}/{brpo,moc}"]
   end
 
   test do
@@ -23,6 +23,19 @@ class Phylobayes < Formula
 end
 
 __END__
+diff --git a/sources/PB.cpp b/sources/PB.cpp
+index d431f5f..c8f249a 100644
+--- a/sources/PB.cpp
++++ b/sources/PB.cpp
+@@ -1093,7 +1093,7 @@ int main(int argc, char* argv[])	{
+ 	}
+ 
+ 	if (randomseed)	{
+-		Random::Random(1001);
++		Random::InitRandom(1001);
+ 	}
+ 	/*
+ 	else	{
 diff --git a/sources/PolyNode.cpp b/sources/PolyNode.cpp
 index f168515..523c636 100644
 --- a/sources/PolyNode.cpp
