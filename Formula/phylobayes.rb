6b28f4d45d4533b015fbf93aa10dfa0c25ba559f
Jonathan Chang <jchang641@gmail.com>
2019-11-22 08:49:57 -0500

    phylobayes 4.1d

--- Formula/phylobayes.rb
+++ Formula/phylobayes.rb
@@ -2,10 +2,8 @@ class Phylobayes < Formula
   # cite Lartillot_2009: "https://doi.org/10.1093/bioinformatics/btp368"
   desc "Phylogenetic reconstruction using infinite mixtures"
   homepage "http://megasun.bch.umontreal.ca/People/lartillot/www/download.html"
-  url "http://megasun.bch.umontreal.ca/People/lartillot/www/phylobayes4.1c.tar.gz"
-  version "4.1c"
-  sha256 "3ab7e853d720537aeeae5c50605abb8559e24221dbc36d97aa02a31753ace943"
-  revision 1
+  url "https://github.com/bayesiancook/phylobayes.git", :revision => "2d3a62c04b86eda28149a94a187580d59ecbff8d"
+  version "4.1d"
 
   bottle do
     root_url "https://dl.bintray.com/jonchang/bottles-biology"
@@ -15,8 +13,6 @@ class Phylobayes < Formula
   end
 
   conflicts_with "phylobayes-mpi"
-
-  patch :DATA
 
   def install
     pkgshare.install Dir["data/*"]
@@ -29,43 +25,3 @@ class Phylobayes < Formula
     system "#{bin}/pb", "-t", "brpo.tree", "-d", "brpo.ali", "-x", "1", "10", "test"
   end
 end
-
-__END__
-diff --git a/sources/PB.cpp b/sources/PB.cpp
-index d431f5f..c8f249a 100644
---- a/sources/PB.cpp
-+++ b/sources/PB.cpp
-@@ -1093,7 +1093,7 @@ int main(int argc, char* argv[])	{
- 	}
- 
- 	if (randomseed)	{
--		Random::Random(1001);
-+		Random::InitRandom(1001);
- 	}
- 	/*
- 	else	{
-diff --git a/sources/PolyNode.cpp b/sources/PolyNode.cpp
-index f168515..523c636 100644
---- a/sources/PolyNode.cpp
-+++ b/sources/PolyNode.cpp
-@@ -470,7 +470,7 @@ string PolyNode::SortLeavesAlphabetical()	{
- 			node = node->next;
- 			degree++;
- 		}	while(node!=down);
--		string retval[degree];
-+		string *retval = new string[degree];
- 		PolyNode* nodelist[degree];
- 		int k = 0;
- 		do	{
-@@ -500,7 +500,9 @@ string PolyNode::SortLeavesAlphabetical()	{
- 		}
- 		nodelist[0]->prev = nodelist[degree-1];
- 		down = nodelist[0];
--		return retval[0];
-+		string new_retval = retval[0];
-+		delete [] retval;
-+		return new_retval;
- 	}	
- 	return "";
- }
-
