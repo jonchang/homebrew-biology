44139e249ee615c6cf7af700ebcf6bc24721dd97
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2019-12-06 03:57:28 +0000

    tact: add 0.2.1 bottle.

--- Formula/tact.rb
+++ Formula/tact.rb
@@ -4,6 +4,13 @@ class Tact < Formula
   url "https://github.com/jonchang/tact/archive/v0.2.1.tar.gz"
   sha256 "03c3cabb3e8889d81f1a3dbc50f3f4507a4399f0142dce00ddcfc13a8219dc72"
 
+  bottle do
+    root_url "https://dl.bintray.com/jonchang/bottles-biology"
+    cellar :any
+    sha256 "2f191bc53a0e70a0efc7174bc5e6a20f19db34126665b7296e2b638e25732428" => :catalina
+    sha256 "f3c5f12e958762493f228daeac9dc5564a3c3aa322f99d9c6d733cedda8a728f" => :x86_64_linux
+  end
+
   depends_on "pipx" => :build
   depends_on "gcc" # for gfortran
   depends_on "python"
