e714f39d8f82aeb9750ff9d84c30bb8d1223d113
Jonathan Chang <jchang641@gmail.com>
2020-01-16 16:43:59 -0800

    bucky: update

--- Formula/bucky.rb
+++ Formula/bucky.rb
@@ -1,11 +1,10 @@
 class Bucky < Formula
+  # cite Larget_2010: "https://doi.org/10.1093/bioinformatics/btq539"
   desc "Bayesian concordance analysis of gene trees"
   homepage "http://www.stat.wisc.edu/~ane/bucky/"
   url "http://www.stat.wisc.edu/~ane/bucky/v1.4/bucky-1.4.4.tgz"
   sha256 "1621fee0d42314d9aa45d0082b358d4531e7d1d1a0089c807c1b21fbdc4e4592"
-  head "http://www.stat.wisc.edu/~ane/bucky.git"
-  # tag "bioinformatics"
-  # doi "10.1093/bioinformatics/btq539"
+  head "https://github.com/jonchang/bucky.git"
 
   def install
     cd "src" do
@@ -13,13 +12,6 @@ class Bucky < Formula
       bin.install "mbsum", "bucky"
     end
     pkgshare.install ["data", "scripts", "doc"]
-  end
-
-  def caveats
-    <<~EOS
-      The manual, examples, and scripts are installed to:
-          #{pkgshare}
-    EOS
   end
 
   test do
