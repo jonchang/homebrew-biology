ae974305375bec77f330e228028548ec8d9da688
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-01-29 01:26:09 +0000

    poy5: add 2020.29.01 bottle.

--- Formula/poy5.rb
+++ Formula/poy5.rb
@@ -4,6 +4,13 @@ class Poy5 < Formula
   url "https://github.com/amnh/poy5.git", :revision => "da563a2339d3fa9c0110ae86cc35fad576f728ab"
   version "2020.29.01"
 
+  bottle do
+    root_url "https://dl.bintray.com/jonchang/bottles-biology"
+    cellar :any_skip_relocation
+    sha256 "ba6c6f5238f00d517ccc815dd61c5e7219d4a1877a338ecb65a0e4ab382542a1" => :catalina
+    sha256 "c94e7b16afe2dc77a0c2e908d9d130b4a621d2f5df65909c30bc3fd5f67504d4" => :x86_64_linux
+  end
+
   depends_on "camlp4" => :build
   depends_on "ocaml" => :build
   depends_on "ocamlbuild" => :build
