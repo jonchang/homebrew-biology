65abf464ab8ebee630843660f8fcc93fd9f4fc0d
Jonathan Chang <jchang641@gmail.com>
2020-06-04 01:37:53 +1000

    scythe: fix style

--- Formula/scythe.rb
+++ Formula/scythe.rb
@@ -5,14 +5,13 @@ class Scythe < Formula
       :revision => "20d3cff7d7f483bd779aff75f861e93708c0a2b5"
   version "0.994"
   head "https://github.com/vsbuffalo/scythe.git"
+
   bottle do
     root_url "https://dl.bintray.com/jonchang/bottles-biology"
     cellar :any_skip_relocation
     sha256 "be1d2171aaed9f971175d104558546ad398acf44846ec00a08ce65fc44019ebb" => :catalina
     sha256 "738d10646a49bcea042e37b33e6a1bb8eaf9652097eb749b153bfa81ae8950a7" => :x86_64_linux
   end
-
-  # tag "bioinformatics"
 
   def install
     system "make", "all"
