28e9ca5110d243dfccd4d5529a9a802ebb833b1c
BrewTestBot <homebrew-test-bot@lists.sfconservancy.org>
2020-06-03 15:35:14 +0000

    phylocom: add 4.2 bottle.

--- Formula/phylocom.rb
+++ Formula/phylocom.rb
@@ -5,6 +5,13 @@ class Phylocom < Formula
   sha256 "f4f111adedfc91f2316b08ec5a994da83888bb1c0acd6076083112a5f51583c7"
   head "https://github.com/phylocom/phylocom.git"
 
+  bottle do
+    root_url "https://dl.bintray.com/jonchang/bottles-biology"
+    cellar :any_skip_relocation
+    sha256 "ded32f4876f63cc84dc39c3ff97e2228ac1b27aacb4fb9187835182409a41df7" => :catalina
+    sha256 "6fefbc1aa15381b61f15050ee8d6ca120cf8e59c13e065667843cc02c440f027" => :x86_64_linux
+  end
+
   def install
     cd "src" do
       system "make"
