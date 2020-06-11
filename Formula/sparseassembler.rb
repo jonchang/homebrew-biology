964f681532fe70355819201d8a21f5e5c74574d0
Jonathan Chang <jchang641@gmail.com>
2020-06-11 18:56:06 +1000

    sparseassembler 20180622

--- Formula/sparseassembler.rb
+++ Formula/sparseassembler.rb
@@ -1,11 +1,12 @@
 class Sparseassembler < Formula
+  # cite Ye_2012: "https://doi.org/10.1186/1471-2105-13-S6-S1"
   desc "Sparse k-mer graph based memory-efficient genome assembler"
   homepage "https://sites.google.com/site/sparseassembler/"
   url "https://github.com/yechengxi/SparseAssembler.git", :revision => "3f802de4862c736b7f0855eff62ca1439b0eb86e"
-  version "20160205"
+  version "20180622"
 
-  resource("testdata") do
-    url "http://www.bcgsc.ca/platform/bioinfo/software/abyss/releases/1.3.4/test-data.tar.gz"
+  resource "testdata" do
+    url "https://www.bcgsc.ca/platform/bioinfo/software/abyss/releases/1.3.4/test-data.tar.gz"
     sha256 "28f8592203daf2d7c3b90887f9344ea54fda39451464a306ef0226224e5f4f0e"
   end
 
