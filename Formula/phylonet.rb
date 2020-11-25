1ffecd366d80e95109114fa524c1b1312c580c6f
Jonathan Chang <me@jonathanchang.org>
2020-11-26 01:57:15 +1100

    phylonet: update for java

--- Formula/phylonet.rb
+++ Formula/phylonet.rb
@@ -3,6 +3,7 @@ class Phylonet < Formula
   homepage "https://bioinfocs.rice.edu/phylonet"
   url "https://bioinfocs.rice.edu/sites/g/files/bxs266/f/kcfinder/files/PhyloNet_3.6.4.jar", using: :nounzip
   sha256 "3919c3408fb5f7af0d465738475cc20d2952eba37e09c51ee3f3ba8df966a8d4"
+  revision 1
 
   bottle :unneeded
   depends_on "openjdk"
