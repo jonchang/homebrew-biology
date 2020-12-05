f4377166c54e26198f88d21b1881ff84f3252604
Jonathan Chang <me@jonathanchang.org>
2020-12-05 13:33:22 +1100

    phylonet: migrate to openjdk

--- Formula/phylonet.rb
+++ Formula/phylonet.rb
@@ -5,7 +5,7 @@ class Phylonet < Formula
   sha256 "3919c3408fb5f7af0d465738475cc20d2952eba37e09c51ee3f3ba8df966a8d4"
 
   bottle :unneeded
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     libexec.install Dir["*.jar"]
