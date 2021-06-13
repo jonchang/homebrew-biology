7f48777d0c6d04bfde51ff171e995602e40a4099
Jonathan Chang <me@jonathanchang.org>
2021-06-12 19:55:21 -0700

    examl: fix test
    Closes #49.
    
    Signed-off-by: Jonathan Chang <me@jonathanchang.org>

--- Formula/examl.rb
+++ Formula/examl.rb
@@ -1,7 +1,7 @@
 class Examl < Formula
   # cite Kozlov_2015: "https://doi.org/10.1093/bioinformatics/btv184"
   desc "Exascale maximum likelihood phylogenetic inference"
-  homepage "http://sco.h-its.org/exelixis/web/software/examl/index.html"
+  homepage "https://cme.h-its.org/exelixis/web/software/examl/index.html"
   url "https://github.com/stamatak/ExaML/archive/v3.0.22.tar.gz"
   sha256 "802e673b0c2ea83fdbe6b060048d83f22b6978933a04be64fb9b4334fe318ca3"
   head "https://github.com/stamatak/ExaML.git"
@@ -31,6 +31,6 @@ class Examl < Formula
     cp pkgshare/"testData/49.tree", testpath
     system "#{bin}/parse-examl", "-s", "49", "-m", "DNA", "-n", "49"
     system "#{bin}/examl", "-t", "49.tree", "-m", "GAMMA", "-s", "49.binary", "-n", "T1"
-    assert_predicate "ExaML_result.T1", :exist?
+    assert_predicate testpath/"ExaML_result.T1", :exist?
   end
 end
